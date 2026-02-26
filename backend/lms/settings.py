5f7788c074e108c551fb749debf9cc65b695e2b7
Jitendra <jitusahuu2002@gmail.com>
2026-02-26 11:42:45 +0530

    commit done

--- backend/lms/settings.py
+++ backend/lms/settings.py
@@ -97,7 +97,9 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
